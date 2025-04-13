3957c4e47e367ac07875a2d102568bed6f9cd691
zhugezifang <xiaoqiu2017wy@163.com>
2025-04-13 09:38:55 +0000

    feat:item

--- templates/tailspark/landing/components/project/index.tsx
+++ templates/tailspark/landing/components/project/index.tsx
@@ -25,9 +25,9 @@ export default ({
       <Crumb category={category} project={project} />
 
       <div className="mx-auto w-full max-w-7xl py-8 md:py-8 lg:py-8">
-        <div className="grid gap-12 sm:gap-20 lg:grid-cols-2 mt-8">
-          <div className="flex flex-col items-start gap-2">
-            <h1 className="text-4xl font-bold md:text-6xl">{project.name}</h1>
+        <div className="mt-8">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-center text-4xl font-bold md:text-6xl">{project.name}</h1>
 
             <div className="flex items-center gap-2 mt-4">
               
@@ -36,10 +36,21 @@ export default ({
             
           </div>
           {project.img_url && (
-            <div className="min-h-96 rounded-md overflow-hidden">
-              <Preview project={project} />
+            <div className="flex flex-col items-center gap-4 p-4">
+            <Preview project={project} />
             </div>
           )}
+
+
+          <div className="flex items-center justify-center py-4">
+              <a href={project.url} target="_blank" className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300 ease-in-out transform hover:-translate-y-1 flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                      <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                  </svg>
+                  下载文件
+              </a>
+          </div>
+
         </div>
       </div>
 
--- templates/tailspark/landing/components/project/preview.tsx
+++ templates/tailspark/landing/components/project/preview.tsx
@@ -4,17 +4,17 @@ export default function ({ project }: { project: Project }) {
   const imgList = project.img_url.split(";");
   return (
 
-    <div>
+   <>
     {imgList.map((imgUrl, index) => (
       <img
         key={index}
         // Bug 修复：将 String 类型转换为 string 类型
         src={imgUrl}
         alt={project.name}
-        className="rounded-md"
+        className="object-cover"
       />
     ))}
-  </div>
+  </>
 
   );
 }

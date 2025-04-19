7d35c33407a50fd16287e6310233011de15b1f6f
诸葛子房 <xiaoqiu2017wy@163.com>
2025-04-19 22:07:44 +0800

    feat:sleep

---
+++ components/download/index.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { sleep } from "openai/core.mjs";
+import React from "react";
+
+export default function DownLoad({ imageUrl,fileName }: { imageUrl: string,fileName:string }) {
+    async function downloadImage(imageUrl:string,fileName:string){
+        await sleep(5000);
+        window.open(imageUrl, "_blank");
+    }
+
+  return (
+    <>
+        <div className="flex items-center justify-center py-4">
+            <div onClick={() => downloadImage(imageUrl, fileName)} className="cursor-pointer px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300 ease-in-out transform hover:-translate-y-1 flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+                下载简历模板
+            </div>
+        </div>
+    </>
+  );
+}
--- templates/tailspark/landing/components/project/index.tsx
+++ templates/tailspark/landing/components/project/index.tsx
@@ -1,6 +1,7 @@
 import { BiCategory } from "react-icons/bi";
 import { BsTags } from "react-icons/bs";
 import { Category } from "@/types/category";
+import DownLoad from "@/components/download";
 import Crumb from "./crumb";
 import Markdown from "@/components/markdown";
 import Preview from "./preview";
@@ -42,14 +43,7 @@ export default ({
           )}
 
 
-          <div className="flex items-center justify-center py-4">
-              <a href={project.url} target="_blank" className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300 ease-in-out transform hover:-translate-y-1 flex items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
-                      <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clip-rule="evenodd" />
-                  </svg>
-                  下载简历模板
-              </a>
-          </div>
+          <DownLoad imageUrl={project.url as string} fileName={project.name as string}/>
 
         </div>
       </div>

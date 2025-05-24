601faf2366c925cb80bd7dcded0894cbea5f749c
诸葛子房 <xiaoqiu2017wy@163.com>
2025-05-24 22:55:22 +0800

    feat:category

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -3,7 +3,7 @@ import {
   getProjectsCount,
   getProjectsWithKeyword,
 } from "@/models/project";
-
+import { getCategories } from "@/models/category";
 import LandingPage from "@/templates/tailspark/landing/pages/index";
 import { Project } from "@/types/project";
 import pagejson from "@/pagejson/en.json";
@@ -23,12 +23,13 @@ export default async function ({
   } else {
     projects = await getFeaturedProjects(1, 45);
   }
-
+  const categories = await getCategories(1, 100);
   const projectsCount = await getProjectsCount();
 
   return (
     <LandingPage
       page={pagejson}
+      categories={categories}
       projects={projects}
       projectsCount={projectsCount}
     />
--- templates/tailspark/landing/pages/index.tsx
+++ templates/tailspark/landing/pages/index.tsx
@@ -4,13 +4,17 @@ import { Page } from "@/types/landing";
 import { Project } from "@/types/project";
 import Projects from "../components/projects";
 import Search from "../components/search";
+import { Category } from "@/types/category";
+import Link from "next/link";
 
 export default function ({
   page,
+  categories,
   projects,
   projectsCount,
 }: {
   page: Page;
+  categories: Category[];
   projects: Project[];
   projectsCount: number;
 }) {
@@ -18,6 +22,22 @@ export default function ({
     <div>
       {page.hero && <Hero hero={page.hero} count={projectsCount} />}
       <Search />
+      <div className="container mx-auto px-4 pt-6 pb-2">
+        <nav className="flex justify-center space-x-6">
+        {categories &&
+          categories.map((category: Category) => {
+            return (
+              <Link
+                key={category.name}
+                href={`/category/${category.name}`}
+                className="px-4 py-2 bg-white rounded-xl p-4 shadow-sm hover:shadow-md transition-shadow cursor-pointer border border-gray-200"
+              >
+                {category.title}
+              </Link>
+            );
+        })}
+        </nav>
+      </div>
       <Projects projects={projects} />
       {page.faq && <Faq section={page.faq} />}
     </div>
